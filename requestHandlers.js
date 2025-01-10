58d22a1ad0899947762000f59e4c890987afc421
jmcho2010 <projm2786@gmail.com>
2025-01-10 10:14:02 +0900

    test

--- requestHandlers.js
+++ requestHandlers.js
@@ -29,25 +29,30 @@ function start(response, postData){
 
 }   
 function upload(response, request){
-    console.log("업로드 함수 실행됨");
-    response.writeHead(200, {"Content-Type" : "text/plain"});
-    response.write("ㅇㅇ" + query.parse(postData).text);
-    response.end();
+    // console.log("업로드 함수 실행됨");
+    
+    // console.log(query.parse(postData).text);
+    // response.writeHead(200, {"Content-Type" : "text/plain"});
+    // response.write("ㅇㅇ" + query.parse(postData).text);
+
+
+    // response.end();
 
     // 업로드 확인용
-    // let form = new formidable.IncomingForm();
-    // console.log("전송확인")
-    // form.parse(request, function(error, fields, files){
-    //     console.log("전송완료");
-    //     // 파일의 이름은 지금처럼 하드코딩하는게 아님(리터럴값으로 쓰면 큰일남)
-    //     // 파일의 이름은 uuid같은 개념을 활용하여
-    //     // 원래 이름과 스토리지 저장용 이름을 따로 구분해두는것이 좋음
-    //     fs.renameSync(files.upload.path, "uploadfiles/test.png");
-    //     response.writeHead(200, {"Content-Type" : "text/html"});
-    //     response.write("received image:<br/>");
-    //     response.write("<img src='/show' />");
-    //     response.end();
-    // });
+    let form = new formidable.IncomingForm();
+    console.log("전송확인")
+    form.parse(request, function(error, fields, files){
+        console.log("전송완료");
+        console.log(files);
+        // 파일의 이름은 지금처럼 하드코딩하는게 아님(리터럴값으로 쓰면 큰일남)
+        // 파일의 이름은 uuid같은 개념을 활용하여
+        // 원래 이름과 스토리지 저장용 이름을 따로 구분해두는것이 좋음
+        fs.renameSync(files.upload.path, "uploadfiles/test.png");
+        response.writeHead(200, {"Content-Type" : "text/html"});
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
 
 }
 
